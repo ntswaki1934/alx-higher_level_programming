4d6bbf2c180a688d47138087ddef285cfb061a26
ntswakikekana <ntswakikekana46@gmail.com>
2024-03-30 06:48:45 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#checks if URL argument is provided
-
-if [$# -ne 1];
-then echo "Usage:$0 <URL>"
-	exit 1
-
+# Check if a URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
 fi
 
-#Get the HTTP response body size using curl
-response_size=$(curl sI "$1" |grep -i 'content-length'|awk '{print $2}'
+# Get the HTTP response body size using curl
+response_size=$(curl -sI "$1" | grep -i 'content-length' | awk '{print $2}')
 
-#Display byte size"
-echo "Response body size:$response_size bytes"
+# Display the size in bytes
+echo "Response body size: $response_size bytes"
+
